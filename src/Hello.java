d0cc98140acff040816edbb2efccfbfdd575a36d
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-15 21:51:02 +0530

    ..

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+class Hello{
+    void print(){
+        System.out.println("Hello! This is day 1 of TNS");
+    }
+}
+class HelloMain{
+    public static void main(String args[]){
+        Hello obj = new Hello();
+        obj.print();
+    }
+}

8ce14cdd628f15014ffc616ba41969d65cad683f
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-15 22:56:47 +0530

    loop practice

---
+++ src/ForLoopDemo.java
@@ -0,0 +1,11 @@
+//For loop demo
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("TNS Training !!");
+		}
+	}
+
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,13 @@
+//demo for while loop
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i=5;
+		while(i>=1)
+		{
+			System.out.println("Hello guys!");
+			i--;
+		}
+	}
+
+}

7a7997e407b9cbe96e6542a3d03ecc2c552914d0
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-15 22:24:18 +0530

    If Else demo

---
+++ src/ConditionalDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ConditionalDemo {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the age: ");
+		int age=sc.nextInt();
+		if(age>=18)
+		{
+			System.out.print(age+" is valid Right to vote");		
+		}
+		else
+		{
+			System.out.println("CANNOT VOTE");
+		}
+		
+
+	}
+
+}

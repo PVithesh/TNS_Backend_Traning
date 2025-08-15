e8f9c5723ccf520a78bffb8e2a590b0849849b3d
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-15 22:00:42 +0530

    Variable Demo

---
+++ src/VariableDemo.java
@@ -0,0 +1,15 @@
+//demo for types of variables
+public class VariableDemo {
+	//instance variable
+	String name;
+	
+	//Static Variable
+	static String college = "KPRIT";
+	
+	void display() {
+		//local variable
+		String nameT = "This is local variable data";
+		System.out.println(nameT);
+	}
+
+}
---
+++ src/VariableDemoMain.java
@@ -0,0 +1,15 @@
+
+public class VariableDemoMain {
+
+	public static void main(String[] args) {
+		
+		VariableDemo obj = new VariableDemo();
+		System.out.println(obj.name = "This is instance Variable");
+		obj.display();
+		
+		//calling static variable
+		System.out.println(VariableDemo.college);
+
+	}
+
+}

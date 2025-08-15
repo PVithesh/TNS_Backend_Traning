f377eb87432741de64da5f85fce5c12815426823
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-15 22:50:26 +0530

    Example

---
+++ src/GreatestNumber.java
@@ -0,0 +1,19 @@
+// program to find the greatest number among three numbers
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		int a = 25;
+        int b = 40;
+        int c = 30;
+
+        if (a >= b && a >= c) {
+            System.out.println("a is greatest: "+a);
+        } else if (b >= c) {
+            System.out.println("b is greatest: "+b);
+        } else {
+            System.out.println("c is greatest: "+c);
+        }
+
+	}
+
+}

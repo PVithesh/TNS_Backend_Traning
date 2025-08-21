4ac2f5daf1cf89b303c7ffdfaed5aac294d1b37e
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-21 16:55:13 +0530

    Polymorphism(OverLoading)

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/MainOverLoad.java
@@ -0,0 +1,13 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//main method
+public class MainOverLoad {
+
+	public static void main(String[] args) {
+		OverLoading obj = new OverLoading();
+		System.out.println(obj.add(10, 50));
+		System.out.println(obj.add(92, 43, 87));
+		System.out.println(obj.add(654.22f,121.34f));
+
+	}
+
+}
---
+++ src/com/kprit/tns/backendtraining/oopsdemo/OverLoading.java
@@ -0,0 +1,16 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//Polymorphism
+//Demo of Over Loading
+
+public class OverLoading {
+	int add(int a,int b) {
+		return a+b;
+	}
+	int add(int a,int b,int c) {
+		return a+b+c;
+	}
+	float add(float a,float b) {
+		return a+b;
+	}
+
+}

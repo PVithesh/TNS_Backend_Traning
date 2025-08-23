7939efc69c72005d602b7a719571361aa234efa3
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-24 00:22:51 +0530

    Abstraction

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/VehicleMain.java
@@ -0,0 +1,15 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+
+public class VehicleMain {
+
+	public static void main(String[] args) {
+		Vehicle ob = new Car();
+		ob.start();
+		ob.stop();
+		Vehicle obj = new Bike();
+		obj.start();
+		obj.stop();
+
+	}
+
+}

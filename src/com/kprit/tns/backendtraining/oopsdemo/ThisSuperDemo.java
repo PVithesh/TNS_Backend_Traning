9c56bb36706773ee266bd5a9a94c9ca9b5d8c502
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-23 22:39:04 +0530

    super and this Keyword

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/ThisSuperDemo.java
@@ -0,0 +1,11 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//demo for super and this keywords
+public class ThisSuperDemo {
+
+	public static void main(String[] args) {
+		Student ob = new Student("Vithesh",874918945,"Hyderabad",234,"CSE");
+		System.out.println(ob);
+	}
+
+
+}

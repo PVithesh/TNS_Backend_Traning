a3882b85bab1c4d3d3cfde8ad06ff50e9a25d343
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-24 00:22:35 +0530

    Abstraction

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/Car.java
@@ -0,0 +1,11 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//implementation class
+public class Car extends Vehicle{
+
+	@Override
+	void start() {
+		System.out.println("The car starts with key ignition ");
+		
+	}
+	
+}

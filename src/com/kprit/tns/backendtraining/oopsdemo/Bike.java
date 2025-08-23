d3a7cbc1c1ee17b03776347985864ae449d4fc94
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-24 00:22:45 +0530

    Abstraction

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/Bike.java
@@ -0,0 +1,11 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+
+public class Bike extends Vehicle{
+
+	@Override
+	void start() {
+		System.out.println("Bike stars with the kick ");
+		
+	}
+	
+}

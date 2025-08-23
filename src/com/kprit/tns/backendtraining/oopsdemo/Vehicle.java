3221c78473a8c144ff7c5a02167e35593aba416a
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-24 00:22:26 +0530

    Abstraction

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/Vehicle.java
@@ -0,0 +1,11 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//demo for abstraction 
+public abstract class Vehicle {
+	abstract void start();
+	
+	void stop() {
+		System.out.println("Vehicle Stopped !!");
+		
+	}
+
+}

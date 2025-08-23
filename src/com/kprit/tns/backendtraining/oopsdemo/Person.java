e90e1e640f2ad2b926d2b249186e3af047d5f9b5
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-23 22:39:13 +0530

    super and this Keyword

---
+++ src/com/kprit/tns/backendtraining/oopsdemo/Person.java
@@ -0,0 +1,13 @@
+package com.kprit.tns.backendtraining.oopsdemo;
+//Parent Class
+public class Person {
+	String pname;
+	long contactno;
+	String address;
+	public Person(String pname,long contactno,String address) {
+		super();
+		this.pname = pname;
+		this.contactno = contactno;
+		this.address = address;
+	}
+}

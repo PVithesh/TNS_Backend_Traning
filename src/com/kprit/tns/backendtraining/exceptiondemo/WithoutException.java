870423f5d9afdded72b418950c58f4172cf3de0c
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-25 23:16:28 +0530

    Exception Handling

---
+++ src/com/kprit/tns/backendtraining/exceptiondemo/WithoutException.java
@@ -0,0 +1,12 @@
+package com.kprit.tns.backendtraining.exceptiondemo;
+//program for without exception
+public class WithoutException {
+
+	public static void main(String[] args) {
+		
+		int b = 12/0;
+		System.out.println("The result is: "+b);
+
+	}
+
+}

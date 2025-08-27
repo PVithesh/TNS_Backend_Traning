8b6ae3ee5dc7eb41e5e7d06d0f4d9cf32d302d69
Pottabathini Vithesh <pottabathinivithesh@gmail.com>
2025-08-27 22:32:00 +0530

    MultiThreading

---
+++ src/com/kprit/tns/backendtraining/multithread/MainThread.java
@@ -0,0 +1,13 @@
+package com.kprit.tns.backendtraining.multithread;
+
+public class MainThread {
+
+	public static void main(String[] args) {
+		ThreadClass ob = new ThreadClass();
+		ob.start();
+		
+		SecondThread obj = new SecondThread();
+		obj.start();
+	}
+
+}

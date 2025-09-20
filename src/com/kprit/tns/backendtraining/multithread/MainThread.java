59f218c97b0c8cecfc0309a15e46b3313fc11406
PVithesh <pottabathinivithesh@gmail.com>
2025-09-20 23:47:24 +0530

    Thread

--- src/com/kprit/tns/backendtraining/multithread/MainThread.java
+++ src/com/kprit/tns/backendtraining/multithread/MainThread.java
@@ -5,7 +5,6 @@ public class MainThread {
 	public static void main(String[] args) {
 		ThreadClass ob = new ThreadClass();
 		ob.start();
-		
 		SecondThread obj = new SecondThread();
 		obj.start();
 	}
